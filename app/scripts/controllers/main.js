48001a810701429a24a6a1b9cf531757ec7bd29a
Nick Stuart <nick@portlandwebworks.com>
2013-03-21 13:15:22 -0400

    Yo intialized.

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('yoWebappApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
